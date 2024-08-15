9d79be94d9188f74b7313feffd1bddebc89b5ea9
Xideta <m.u.hermansen@gmail.com>
2024-08-15 23:21:36 +0200

    Update LibRangeCheck-3.0.lua

--- Libs/LibRangeCheck-3.0/LibRangeCheck-3.0.lua
+++ Libs/LibRangeCheck-3.0/LibRangeCheck-3.0.lua
@@ -634,7 +634,8 @@ local function initItemRequests(cacheAll)
 end
 
 local function getNumSpells()
-  local _, _, offset, numSpells = C_SpellBook.GetSpellBookSkillLineInfo(C_SpellBook.GetNumSpellBookSkillLines())
+  local info = C_SpellBook.GetSpellBookSkillLineInfo(C_SpellBook.GetNumSpellBookSkillLines());
+  local offset, numSpells = info.offset, info.numSpellBookItems;
   return offset + numSpells
 end
 
